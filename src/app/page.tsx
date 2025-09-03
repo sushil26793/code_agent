58d8b99c19fef9f87db96d211217c2d1e167e002
sushil satyarthi <sushilsatyarthi@gmail.com>
2025-09-03 20:19:02 +0530

    gemini-ai-integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 "use client"
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 import { toast } from "sonner";
 
 export default   function Home() {
+  const [value,setValue]=useState('')
   const trpc= useTRPC();
   const invoke= useMutation(trpc.invoke.mutationOptions({
     onSuccess:()=>{
@@ -14,7 +17,8 @@ export default   function Home() {
   }))
   return (
     <div className="p-4 max-w-7xl mx-auto">
-        <Button disabled={invoke.isPending} onClick={()=>invoke.mutate({text:"test john"})}>Invoke background job</Button>
+      <Input value={value} onChange={(e)=>setValue(e.target.value)}/>
+        <Button disabled={invoke.isPending} onClick={()=>invoke.mutate({value})}>Invoke background job</Button>
        </div>
   );
 }
