73df03592d473cea7a446e0c8759642612d48a4c
sushil satyarthi <sushilsatyarthi@gmail.com>
2025-09-06 18:06:45 +0530

    eb2-sandbox-setup-and-dockerization

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,16 @@
 import { inngest } from "./client";
-import {gemini,createAgent } from "@inngest/agent-kit"
+import {gemini,createAgent } from "@inngest/agent-kit";
+import {Sandbox} from "@e2b/code-interpreter"
+import { getSandbox } from "./utills";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
+    const sandboxId= await step.run("get-sandbox-id",async ()=>{
+      const sandbox = await Sandbox.create("vibe-nextjs-sushil-01")
+      return sandbox.sandboxId;
+    })
     await step.sleep("wait-a-moment", "5s");
 
     const codeAgent = createAgent({
@@ -12,9 +18,12 @@ export const helloWorld = inngest.createFunction(
       system: "You are an expert MERN stack (next js) developer . You write readable , maintainable code. You write simple Next.js , React.js snippets.",
       model: gemini({ model: "gemini-2.0-flash"}),
     });
-    const { output } = await codeAgent.run(
-      `Write the following code snippet: ${event.data.value}`,
-);
-    return { output };
+    const { output } = await codeAgent.run(`Write the following code snippet: ${event.data.value}`,);
+    const sandboxUrl= await step.run("get-sandbox-url",async ()=>{
+      const sandbox = await getSandbox(sandboxId);
+      const host= sandbox.getHost(3000);
+      return `https://${host}`;
+    })
+    return { output,sandboxUrl  };
   },
 );
