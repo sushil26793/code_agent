f46595c30937d0c225674260f34648de488e9336
sushil satyarthi <sushilsatyarthi@gmail.com>
2025-09-26 16:20:29 +0530

    10: message-ui-design

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -1,3 +1,7 @@
+import { ProjectView } from "@/modules/projects/ui/views/project-view";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
 
 interface Props {
     params: Promise<{
@@ -7,8 +11,16 @@ interface Props {
 
 const Page = async ({ params }: Props) => {
     const { projectId } = await params;
+    const queryClient = getQueryClient();
+    void queryClient.prefetchQuery(trpc.messages.getMany.queryOptions({ projectId }))
+    void queryClient.prefetchQuery(trpc.projects.getOne.queryOptions({ id: projectId }))
+
     return (
-        <div>Project Id: {projectId}</div>
+        <HydrationBoundary state={dehydrate(queryClient)}>
+            <Suspense fallback={<p>Loading</p>}>
+            <ProjectView projectId={projectId} />
+            </Suspense>
+        </HydrationBoundary>
     );
 };
 
--- src/modules/projects/server/procedures.ts
+++ src/modules/projects/server/procedures.ts
@@ -3,10 +3,24 @@ import { prisma } from "@/lib/db";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import z from "zod";
 import { generateSlug } from "random-word-slugs";
-
+import {TRPCError} from "@trpc/server";
 
 
 export const projectRouter = createTRPCRouter({
+     getOne: baseProcedure
+     .input(z.object({
+        id: z.string()
+     }))
+     .query(async ({ input }) => {
+        const project = await prisma.project.findUnique({
+            where: { id: input.id },
+            include: {
+                messages: true
+            }
+        })
+        if(!project) throw new TRPCError({ code:"NOT_FOUND", message: "Project not found" });
+        return project;
+    }),
     getMany: baseProcedure.query(async () => {
         const projects = await prisma.project.findMany({
             orderBy: { createdAt: "desc" },
